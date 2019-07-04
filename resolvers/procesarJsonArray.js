6d7ad045e5463cfcaf99b82f9a2c5a50f35705f5
Federico <fede.maidan@gmail.com>
2019-07-04 17:12:30 -0300

    settimeout

--- resolvers/procesarJsonArray.js
+++ resolvers/procesarJsonArray.js
@@ -1,6 +1,7 @@
 const procesarJson = require('./procesarJson')
 
 module.exports = (jsonArray, desde, hasta) => {
+	console.log("pase")
 	for (var i = desde; i < hasta; i++) {
 		var json = jsonArray[i]
 		procesarJson(json,i,jsonArray, i+1 == hasta)
