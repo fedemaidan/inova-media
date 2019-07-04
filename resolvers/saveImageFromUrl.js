67d041f2c890c50ea3bb4cfd55a85efeddeda114
Federico <fede.maidan@gmail.com>
2019-07-04 17:06:44 -0300

    settimeout

--- resolvers/saveImageFromUrl.js
+++ resolvers/saveImageFromUrl.js
@@ -3,6 +3,7 @@ var aws = require('aws-sdk');
 const addErrorLine = require('./addErrorLine')
 const addFinalLine = require('./addFinalLine')
 var path = require('path')
+const procesarJsonArray = require('./procesarJsonArray')
 
 var s3 = new aws.S3({
   accessKeyId: process.env.accessKeyId,
@@ -44,50 +45,3 @@ module.exports = (url, nombre, pos, jsonArray, ultimo) => {
     });
 }
 
-
-
-function procesarJsonArray(jsonArray, desde, hasta) {
-    for (var i = desde; i < hasta; i++) {
-        var json = jsonArray[i]
-        procesarJson(json,i,jsonArray, i+1 == hasta)
-    }    
-}
-
-
-function procesarJson(json, pos, jsonArray, ultimo) {
-
-        try {
-            var codigo = json["SKU"]
-            var titulo = json["TITULO"].replace(/\s/g,'').replace(/['"]+/g, ''); 
-
-
-            if (!titulo)
-                titulo = "titulo"
-
-            var csvRow = codigo+","+json["TITULO"]
-
-            for (var imagen = 1; imagen <= 10; imagen++) {
-                var urlAntigua = json[imagen.toString()]
-
-                if (urlAntigua) {
-                    var nombreImagen = codigo+"/"+titulo+"/"+imagen.toString()+path.extname(urlAntigua)
-                    saveImageFromUrl(urlAntigua,nombreImagen,pos, jsonArray, ultimo)
-                    var urlNueva = "https://inova-media.s3.amazonaws.com/"+nombreImagen
-                    csvRow = ","+ urlNueva
-                }
-                else {
-                    csvRow += ","
-                }
-            }
-        } catch (e) {
-            addErrorLine(json["SKU"], "Falló obteniendo datos imagen", e)
-        } finally {
-            fs.appendFile("cargas/ultima.csv", csvRow, "utf8", (err) =>{ 
-                  if (err) console.log(err);
-                        
-                fs.appendFile("cargas/todas.csv", csvRow, "utf8", (err) =>{
-                    console.log("Se escribio el CSV total");
-                })
-        })
-    }
-}
